709ac3b9d0b1847f0288acfc9af210d24e19bdb5
Martin Page <wookoouk@gmail.com>
2015-09-29 15:54:41 +0100

    fixed skipping

--- test/ena.js
+++ test/ena.js
@@ -3,19 +3,18 @@ var path = require('path');
 var ena = require('../lib/ena');
 var config = require('../config').ena;
 
+var canLogIn = config.username.length < 1 && config.password.length < 1;
+
 
 describe('ENA', function () {
-
-  if (config.username.length < 1 && config.password.length < 1) {
-    this.skip();
-  }
-
+  
   describe('.upload', function () {
 
     var fileToUpload = path.join(__dirname, '../hog.png');
     var fileName = 'hog.png';
 
     it('should upload ok', function (done) {
+      if (!canLogIn) this.skip();
       ena.upload(fileToUpload, fileName, function (err) {
         if (err) {
           done(err);
@@ -25,9 +24,10 @@ describe('ENA', function () {
       });
     })
   });
-
   describe('.submit', function () {
+
     it('should upload ok', function (done) {
+      if (!canLogIn) this.skip();
       done(); //TODO
       //ena.submit(SUBMISSION, STUDY, SAMPLE, EXPERIMENT, RUN, ANALYSIS, DAC, POLICY, DATASET, PROJECT);
     })
