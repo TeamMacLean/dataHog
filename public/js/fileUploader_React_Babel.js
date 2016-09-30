f5968eb9228be9fdf2c69aa9d514cb23358e4627
Martin Page <wookoouk@gmail.com>
2016-09-30 16:53:46 +0100

    test

--- public/js/fileUploader_React_Babel.js
+++ public/js/fileUploader_React_Babel.js
@@ -3,7 +3,7 @@
 function fileUploader(mountNode, MD5S, fileID, MD5ID) {
 
 
-    var CHUNK_SIZE = 1024 * 100; //100kb
+    var CHUNK_SIZE = 1024 * 1000;
 
     var socket = io(window.location.host);
     var Files = {};
