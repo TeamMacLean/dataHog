a054011adbcba57b3cf20d638e17fa78e20aa7eb
Martin Page <wookoouk@gmail.com>
2016-11-17 10:17:36 +0000

    main

--- public/js/main.js
+++ public/js/main.js
@@ -1,24 +1,29 @@
-var modal = document.getElementById('md5Modal');
+$(function () {
+
+
+    var modal = document.getElementById('md5Modal');
 
 // Get the button that opens the modal
-var btn = document.getElementsByClassName("md5ModalButton");
+    var btn = document.getElementsByClassName("md5ModalButton");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+    var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
-btn.onclick = function() {
-    modal.style.display = "block";
-};
+    btn.onclick = function () {
+        modal.style.display = "block";
+    };
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-};
+    span.onclick = function () {
+        modal.style.display = "none";
+    };
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-};
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    };
+
+})
